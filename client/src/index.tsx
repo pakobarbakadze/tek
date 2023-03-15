c48a60ae555ca9c385c83bb886407c03fbf0b431
pakobarbakadze <pakobarbakadze@gmail.com>
2023-03-15 11:55:10 +0400

    Redux store.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
 
 import axios from "axios";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 
 axios.defaults.baseURL = process.env.REACT_APP_AXIOS_URI;
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/redux/cartSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+
+import { ProductType } from "../types/Product.types";
+
+interface CartState {
+  data: ProductType[];
+}
+
+const initialState: CartState = {
+  data: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    addProduct: (state, action: PayloadAction<ProductType>) => {
+      state.data.push(action.payload);
+    },
+  },
+});
+
+export const { addProduct } = cartSlice.actions;
+
+export const selectCart = (state: RootState) => state.cart.data;
+
+export default cartSlice.reducer;
